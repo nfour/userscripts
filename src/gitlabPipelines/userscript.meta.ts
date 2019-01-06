c1d920f1a2c448587d875d62878fecb802157f16
nfour <novus.nfour@gmail.com>
2019-01-06 19:47:02 +1000

    Fix meta

--- src/gitlabPipelines/userscript.meta.ts
+++ src/gitlabPipelines/userscript.meta.ts
@@ -3,7 +3,7 @@ import { IMetaSchema } from '../types/meta';
 export default <IMetaSchema> {
   name: 'gitlabPipelines',
   version: '0.0.1',
-  namespace: 'asdsa',
-  description: 'as',
+  namespace: 'nfour',
+  description: 'Shows job output when interacting with MR job dropdowns',
   include: ['*src.temando.io/*', '*gitlab.com*'],
 };
