c7d4d96eceeb7c48f092de91ec3eed0256124cf3
nfour <novus.nfour@gmail.com>
2019-01-07 12:48:06 +1000

    Commit build

---
+++ build/gitlabPipelines.dev.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        gitlabPipelines
+// @version     0.0.1
+// @namespace   nfour
+// @description Shows job output when interacting with MR job dropdowns
+// @include     *src.temando.io/*
+// @include     *gitlab.com*
+// @require     file:///home/sam/W/tsUserscripts/build/gitlabPipelines.js
+// ==/UserScript==
